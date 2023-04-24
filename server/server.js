55be6547fa26f4bd588cd1055da614315bc6e8b5
momviseu software developer <130023862+viseu-developer@users.noreply.github.com>
2023-04-24 20:49:57 +0700

    ecom1

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const ConnectDB = require('./config/db');
+const {readdirSync} = require('fs');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+const morgan = require('morgan');
+const port = process.env.PORT;
+const authRoutes = require("./routes/authRoute");
+const categoryRoutes = require("./routes/categoryRoutes");
+const productRoutes = require("./routes/productRoutes");
+
+
+ConnectDB();
+
+
+//middelwares
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+// //routes
+// app.use("/api/auth", authRoutes);
+// app.use("/api/category", categoryRoutes);
+// app.use("/api/product", productRoutes);
+
+// console.log(readdirSync("./routes"))
+readdirSync("./routes").map((file)=>app.use("/api/category", require("./routes/"+file)));
+readdirSync("./routes").map((file)=>app.use("/api/auth", require("./routes/"+file)));
+readdirSync("./routes").map((file)=>app.use("/api/product", require("./routes/"+file)));
+
+
+
+app.listen(port, console.log("running server on port " + port));
+
+app.use('/', (req, res) => {
+    res.send("hello11");
+})
